6de7d0ff5186f92c58b8fdbf1495a826173cf1e9
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-13 23:37:17 -0300

    estilizando quadrados que viraram circulos e ajustando margin dos inputs

--- mapScript.js
+++ mapScript.js
@@ -1,9 +1,9 @@
-let map = L.map('mapid').setView([51.505, -0.09], 13);
+let map = L.map('mapid').setView([-23.5813856, -46.66364], 12.75);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([-23.5813856, -46.66364]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
